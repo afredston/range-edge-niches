04f2587d38c95c3f5e539f3a99a4b631ed0da945
Alexa Fredston-Hermann <alexa.fredstonhermann@gmail.com>
2021-02-23 10:31:42 -0500

    more plot edits

--- figure-scripts/maps_and_regional_sst_time.R
+++ figure-scripts/maps_and_regional_sst_time.R
@@ -71,7 +71,7 @@ neusgg <- neus_sst_summary %>%
   geom_line(size=1.1) +
   scale_color_manual(values=c("#DF2301","#FF8E02","#3A4ED0")) +
   scale_x_continuous(limits = c(1967, 2018), breaks=seq(1968, 2017, 4), expand = c(0, 0)) +
-  labs(x="Year", y="Temperature (°C)") +
+  labs(x="Year", y="SST (°C)") +
   theme_bw() +
   theme(text=element_text(family="sans",size=12,color="black"),
         legend.position="none",
@@ -89,7 +89,7 @@ wcgg <- wc_sst_summary %>%
   geom_line(size=1.1) +
   scale_color_manual(values=c("#DF2301","#FF8E02","#3A4ED0")) +
   scale_x_continuous(limits = c(1976, 2018), breaks=seq(1976, 2017, 4), expand = c(0, 0)) +
-  labs(x="Year", y="Temperature (°C)") +
+  labs(x="Year", y="SST (°C)") +
   theme_bw() +
   theme(text=element_text(family="sans",size=12,color="black"),
         legend.position="none",
@@ -107,7 +107,7 @@ ebsgg <- ebs_sst_summary %>%
   geom_line(size=1.1) +
   scale_color_manual(values=c("#DF2301","#FF8E02","#3A4ED0")) +
   scale_x_continuous(limits = c(1988, 2018), breaks=seq(1988, 2017, 4), expand = c(0, 0)) +
-  labs(x="Year", y="Temperature (°C)") +
+  labs(x="Year", y="SST (°C)") +
   theme_bw() +
   theme(text=element_text(family="sans",size=12,color="black"),
         legend.position="none",
@@ -119,9 +119,9 @@ ebsgg <- ebs_sst_summary %>%
     ) + 
   NULL
 
-ggsave(neusgg, filename=here("results","neus_sst_inset_plot.png"), height=25, width=25, units="mm", scale=2, dpi=600)
-ggsave(wcgg, filename=here("results","wc_sst_inset_plot.png"), height=25, width=25, scale=2,units="mm",dpi=600)
-ggsave(ebsgg, filename=here("results","ebs_sst_inset_plot.png"), height=25, width=25, scale=2,units="mm", dpi=600)
+ggsave(neusgg, filename=here("results","neus_sst_inset_plot.png"), height=25, width=31, units="mm", scale=1.7, dpi=600)
+ggsave(wcgg, filename=here("results","wc_sst_inset_plot.png"), height=25, width=31, scale=1.7,units="mm",dpi=600)
+ggsave(ebsgg, filename=here("results","ebs_sst_inset_plot.png"), height=25, width=31, scale=1.7,units="mm", dpi=600)
 
 # get rid of whitespace
 plot_crop(here("results","neus_sst_inset_plot.png"))
@@ -182,18 +182,56 @@ ebs.origin <- data.frame(ebs.lon[2], ebs.lat[2]) %>%
   st_cast("POINT") %>%
   st_set_crs(st_crs(ebs_bathy))
 
+# make waypoints
+neus.coastdistrefs <- neus_coastdistdat %>% 
+  filter(seglength > 0,
+         seglength < 100000) %>% # get rid of weird points
+  mutate(coastdist = lengthfromhere/1000,
+         coastdistround = round(coastdist, digits = -2), # round to nearest hundred 
+         coastdistdiff = abs(coastdist-coastdistround)) %>% 
+  group_by(coastdistround) %>% 
+  filter(coastdistdiff == min(coastdistdiff), # get points closest to rounded waypoint value
+         coastdistround <= 1400) %>% 
+  ungroup() %>% 
+  dplyr::select(x, y, coastdistround)
+
+wc.coastdistrefs <- wc_coastdistdat %>% 
+  mutate(coastdist = lengthfromhere/1000,
+         coastdistround = round(coastdist, digits = -2), # round to nearest hundred 
+         coastdistdiff = abs(coastdist-coastdistround)) %>% 
+  group_by(coastdistround) %>% 
+  filter(coastdistdiff == min(coastdistdiff), 
+         coastdistround > 0) %>% # don't need to label the origin
+  ungroup() %>% 
+  dplyr::select(x, y, coastdistround) %>% 
+  filter(coastdistround %in% seq(200, 2000, 200)) # pare down to every 200 for plotting 
+
+ebs.linedistrefs <- readRDS(here("processed-data","ebs_axisdistdat.rds"))%>% 
+  mutate(linedist = lengthfromhere/1000,
+         linedistround = round(linedist, digits = -2), # round to nearest hundred 
+         linedistdiff = abs(linedist-linedistround)) %>% 
+  group_by(linedistround) %>% 
+  filter(linedistdiff == min(linedistdiff), 
+         linedistround > 0) %>% # don't need to label the origin
+  ungroup() %>% 
+  dplyr::select(x, y, linedistround)
+
+# make maps 
 neusmap <- ggplot() + 
   geom_sf(data=neus_bathy, color="skyblue3") +
   geom_sf(data=usoutline, color="#999999") +
   geom_sf(data=neus_coast, color="black", linetype="dashed", lwd=1.5) +
   geom_sf(data=neus.origin, color="black", fill="transparent",shape=4, size=4, stroke=4)+
-  scale_x_continuous(limits = neus_lonrange, expand = c(0, 0)) +
+  geom_point(data=neus.coastdistrefs, aes(x=x, y=y), color="white") +
+  geom_text(data=neus.coastdistrefs, aes(x=x, y=y, label=coastdistround),hjust=0, nudge_x = 0.5, fontface="bold", size=3) +
+  scale_x_continuous(limits = c(neus_lonrange[1], neus_lonrange[2]+1), expand = c(0, 0)) + # add some extra space for labels
   scale_y_continuous(limits = neus_latrange, expand = c(0, 0)) +
   theme_bw() +
   theme(legend.position = "none",
         text=element_text(family="sans",size=12,color="black"),
         legend.text = element_text(size=12),
         axis.text=element_text(family="sans",size=8,color="black"), 
+        axis.text.x = element_text(angle = 45, hjust = 1),
         axis.title=element_blank()) +
   NULL
 
@@ -202,8 +240,10 @@ wcmap <- ggplot() +
   geom_sf(data=usoutline, color="#999999") +
   geom_sf(data=wc_coast, color="black", linetype="dashed", lwd=1.5) +
   geom_sf(data=wc.origin, color="black", fill="transparent",shape=4, size=4, stroke=4)+
-  scale_x_continuous(limits = wc_lonrange, expand = c(0, 0)) +
-  scale_y_continuous(limits = wc_latrange, expand = c(0, 0)) +
+  geom_point(data=wc.coastdistrefs, aes(x=x, y=y), color="white") +
+  geom_text(data=wc.coastdistrefs, aes(x=x, y=y, label=coastdistround),hjust=0, nudge_x = 0.5, fontface="bold", size=3) + 
+  scale_x_continuous(limits = c(wc_lonrange[1], wc_lonrange[2]+1), expand = c(0, 0)) + # add extra space for labels
+  scale_y_continuous(limits = wc_latrange, expand = c(0, 0), breaks = seq(32, 50, 2)) +
   theme_bw() +
   theme(legend.position = "none",
         text=element_text(family="sans",size=12,color="black"),
@@ -218,6 +258,8 @@ ebsmap <- ggplot() +
   geom_sf(data=usoutline, color="#999999") +
   geom_sf(data=ebs.line, color="black", linetype="dashed", lwd=1.5) +
   geom_sf(data=ebs.origin, color="black", fill="transparent",shape=4, size=4, stroke=4)+
+  geom_point(data=ebs.linedistrefs, aes(x=x, y=y), color="white") +
+  geom_text(data=ebs.linedistrefs, aes(x=x, y=y, label=linedistround), nudge_x = -1.85, fontface="bold", size=3) + 
   scale_x_continuous(limits = ebs_lonrange, expand = c(0, 0), breaks=seq(-180, -154, 2)) +
   scale_y_continuous(limits = ebs_latrange, expand = c(0, 0), breaks=seq(54, 66, 2)) +
   theme_bw() +
@@ -229,9 +271,9 @@ ebsmap <- ggplot() +
         axis.text.x = element_text(angle=45, hjust=1)) +
   NULL
 
-ggsave(neusmap, filename=here("results","region_map_neus.png"), height=80, units="mm", dpi=600)
-ggsave(wcmap, filename=here("results","region_map_wc.png"), height=80, units="mm", dpi=600)
-ggsave(ebsmap, filename=here("results","region_map_ebs.png"), dpi=600, height=80, units="mm")
+ggsave(neusmap, filename=here("results","region_map_neus.png"), width=35, units="mm", dpi=600, scale=1.6)
+ggsave(wcmap, filename=here("results","region_map_wc.png"), width=30, units="mm", dpi=600, scale=1.6)
+ggsave(ebsmap, filename=here("results","region_map_ebs.png"), dpi=600, width=40, units="mm", scale=1.6)
 
 # get rid of unwanted whitespace
 plot_crop(here("results","region_map_ebs.png"))
